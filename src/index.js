cb40b784c7961a6d3887eeeaf5966177e7bb9c61
Michelle958 <michelle4.dup@gmail.com>
2025-06-22 17:44:10 +0200

    Update CSS to scale up icons and render live forecast content

--- src/index.js
+++ src/index.js
@@ -56,27 +56,36 @@ searchFormElement.addEventListener("submit", handleSearchSubmit);
 function getForecast(city) {
   let apiKey = "d374t43e29a36b438a5593oaba8810fc";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
- axios(apiUrl).then(displayForecast);
-  
+  axios(apiUrl).then(displayForecast);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
 }
 
 function displayForecast(response) {
-  console.log(response.data)
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml += ` 
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml += ` 
     <div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">🌥️</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
     <div class="weather-forecast-temperatures">
     <div class="weather-forecast-temperature">
-    <strong>15°</strong>
+   <strong>${Math.round(day.temperature.maximum)}°</strong>
     </div>
-    <div class="weather-forecast-temperature">9°</div>
+    <div class="weather-forecast-temperature">${Math.round(
+      day.temperature.minimum
+    )}°</div>
     </div>
     </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
@@ -84,5 +93,3 @@ function displayForecast(response) {
 }
 
 searchCity("Paris");
-
-displayForecast();
